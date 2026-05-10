01c99ea1be7b8a2c383a3620b5c9c2222012acc8
katy1234888 <katyayani.kul@gmail.com>
2026-05-10 10:55:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer, SimpleImputer
 import plotly.express as px
+import plotly.graph_objects as go
 
 # Set Page Config
 st.set_page_config(page_title="Seashells Logistics Dashboard", layout="wide")
